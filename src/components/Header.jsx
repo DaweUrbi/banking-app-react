374557367bf804c17567798915ab0c1e08be9b37
DaweUrbi <urban.da@icloud.com>
2022-07-27 21:39:56 -0700

    Header react

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { StyledHeader } from './styles/Header.styles';
+
+export default function Header() {
+  return (
+    <StyledHeader>
+      <h1>Banking APP (React JS)</h1>
+    </StyledHeader>
+  );
+}
